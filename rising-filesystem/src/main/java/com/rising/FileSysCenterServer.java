ec45563cac67bc6995ea36c297580757ea0e8735
meet <478668865@qq.com>
2021-01-19 09:46:46 +0800

    文件服务

--- rising-filesystem/src/main/java/com/rising/FileSysCenterServer.java
+++ rising-filesystem/src/main/java/com/rising/FileSysCenterServer.java
@@ -1,11 +1,14 @@
 package com.rising;
 
+
+import io.github.bluemiaomiao.annotation.EnableFastdfsClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableFastdfsClient
 @SpringBootApplication
 @EnableEurekaClient
 @EnableResourceServer
