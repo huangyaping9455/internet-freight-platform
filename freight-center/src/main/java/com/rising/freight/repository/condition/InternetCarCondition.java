d732c6b7643003c6f4d2a2a5df169af8c1d3c923
puxiaoxian <1006845942@qq.com>
2021-01-28 15:15:10 +0800

    car

--- freight-center/src/main/java/com/rising/freight/repository/condition/InternetCarCondition.java
+++ freight-center/src/main/java/com/rising/freight/repository/condition/InternetCarCondition.java
@@ -6,5 +6,7 @@ import lombok.Data;
 public class InternetCarCondition {
     private String vehicleNumber;
     private String owner;
+    private String drivingPermit;;
+    private String driverLicense;
 
 }
